87832dacc0daf827578db896b02bf7649a459012
Vladislav B. <vlad6226@gmail.com>
2015-05-17 14:13:21 +1000

    Initial commit

---
+++ DSReader/Reader.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+using com.dalsemi.onewire;
+using com.dalsemi.onewire.adapter;
+using com.dalsemi.onewire.application.monitor;
+
+namespace DSReader
+{
+    // COM interface
+    [Guid("E332DF33-8AAF-4BBD-830A-5C3C8E9E5F5F")]
+    public interface DSReader_Interface
+    {
+        [DispId(1)]
+        long GetID(int timeout = 10);
+    }
+
+    [Guid("6D7DDED5-E94F-43BC-B729-767E9835FA41"),
+        ClassInterface(ClassInterfaceType.None),
+        ComSourceInterfaces(typeof(DSReader_Events))]
+    public class Reader : DSReader_Interface
+    {
+        // @timeout -  time in seconds during which network gets polled
+        // returns unsigned 64-bit ID of first iButton detected during selected timeframe
+        // returns 0 if no iButton device was presented)
+        // return -1 if an exception is raised (1-Wire network not available, most likely driver misconfig)
+        public long GetID(int timeout = 10)
+        {
+            try
+            {
+                // adapter init. 1-Wire Default Device must be selected in driver beforehand!
+                DSPortAdapter adapter = OneWireAccessProvider.getDefaultAdapter();
+                DeviceMonitor dMonitor;
+
+                // vectors for J# interop
+                java.util.Vector arrivals = new java.util.Vector();
+                java.util.Vector departures = new java.util.Vector();
+
+                // get exclusive use of adapter
+                adapter.beginExclusive(true);
+
+                // clear any previous search restrictions
+                adapter.setSearchAllDevices();
+                adapter.targetAllFamilies();
+                adapter.setSpeed(DSPortAdapter.SPEED_REGULAR);
+
+                // release exclusive use of adapter
+                adapter.endExclusive();
+
+                // Monitor of the network
+                dMonitor = new DeviceMonitor(adapter);
+                dMonitor.setDoAlarmSearch(false);
+
+                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+
+                // enforcing internal timeout
+                while (sw.Elapsed <= TimeSpan.FromSeconds(timeout))
+                {
+                    dMonitor.search(arrivals, departures);
+
+                    if (arrivals.size() != 0)
+                    {
+                        // found device, returning it's address as unsigned long
+                        return ((java.lang.Long)arrivals.firstElement()).longValue();
+                    }
+                }
+
+                // no iButton detected
+                return 0;
+            }
+            catch (Exception)
+            {
+                // 1-Wire Net is unavailiable (no receptor detected/driver misconfiguration)
+                return -1;
+            }
+            
+        }
+    }
+
+
+    [Guid("A653C8C0-7D93-4B85-B503-57665E2D7A93")]
+    public interface DSReader_Events
+    {
+
+    }
+}
---
+++ DSReaderSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DSReader;
+
+namespace DSReaderSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to get iButton ID");
+            Console.ReadKey();
+
+        getID:
+            Console.WriteLine();
+            Console.WriteLine("Touch your iButton device to a receptor. You have 10 seconds");
+            
+            var ID = new Reader().GetID();
+
+            switch (ID)
+            {
+                case 0:
+                    Console.WriteLine("iButton device wasn't found");
+                    break;
+                case -1:
+                    Console.WriteLine("1-Wire Net isn't availiable. Please set up default 1-Wire device in driver settings.");
+                    break;
+                default:
+                    Console.WriteLine("ID of presented iButton is {0:X}", ID);
+                    break;
+            }
+
+            Console.Write("Try again? (y/N) ");
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+                goto getID;
+        }
+    }
+}
