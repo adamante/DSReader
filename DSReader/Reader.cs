d58c3f8773c62d3b85263e2d398ca6f222c83d72
Vladislav B. <vlad6226@gmail.com>
2015-05-27 11:17:33 +1000

    Interface type changes. Interop re-work

--- DSReader/Reader.cs
+++ DSReader/Reader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.EnterpriseServices;
 
 using com.dalsemi.onewire;
 using com.dalsemi.onewire.adapter;
@@ -10,77 +11,28 @@ using com.dalsemi.onewire.application.monitor;
 
 namespace DSReader
 {
-    [ComImport]
-    [Guid("00020400-0000-0000-C000-000000000046")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IDispatch
-    {
-        [PreserveSig]
-        int GetTypeInfoCount(out int Count);
-
-        [PreserveSig]
-        int GetTypeInfo
-        (
-          [MarshalAs(UnmanagedType.U4)] int iTInfo,
-          [MarshalAs(UnmanagedType.U4)] int lcid,
-          out System.Runtime.InteropServices.ComTypes.ITypeInfo typeInfo
-        );
-
-        [PreserveSig]
-        int GetIDsOfNames
-        (
-          ref Guid riid,
-          [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr)]
-    string[] rgsNames,
-          int cNames,
-          int lcid,
-          [MarshalAs(UnmanagedType.LPArray)] int[] rgDispId
-        );
-
-        [PreserveSig]
-        int Invoke
-        (
-          int dispIdMember,
-          ref Guid riid,
-          uint lcid,
-          ushort wFlags,
-          ref System.Runtime.InteropServices.ComTypes.DISPPARAMS pDispParams,
-          out object pVarResult,
-          ref System.Runtime.InteropServices.ComTypes.EXCEPINFO pExcepInfo,
-          out UInt32 pArgErr
-        );
-    }
-
-
     // COM interface
     [Guid("E332DF33-8AAF-4BBD-830A-5C3C8E9E5F5F")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface DSReader_Interface
     {
-        [DispId(1)]
         long GetID(int timeout = 10);
     }
 
-    [Guid("E29CEEE7-EDA9-48A3-8045-D8735B16761C")]
+    [Guid("AB634001-F13D-11d0-A459-004095E1DAEA")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface IInitDone
     {
-        [DispId(1)]
-        int Init(IDispatch pBackConnection);
-        
-        [DispId(2)]
-        int Done();
+        void Init([MarshalAs(UnmanagedType.IDispatch)] object pBackConnection);
 
-        [DispId(3)]
-        int GetInfo([MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_I4)] ref IntPtr pInfo);
+        void Done();
+
+        void GetInfo([MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref object[] pInfo);
     }
 
     [Guid("6D7DDED5-E94F-43BC-B729-767E9835FA41"),
-        ClassInterface(ClassInterfaceType.None),
-        ComSourceInterfaces(typeof(DSReader_Events))]
-    [ProgId("AddIn.DSReader")]
+        ClassInterface(ClassInterfaceType.AutoDual)]
     public class Reader : DSReader_Interface, IInitDone
     {
-        public const int S_OK = unchecked((int)0x00000000);
         // @timeout -  time in seconds during which network gets polled
         // returns unsigned 64-bit ID of first iButton detected during selected timeframe
         // returns 0 if no iButton device was presented)
@@ -138,38 +90,25 @@ namespace DSReader
             
         }
 
+        public Reader()
+        { }
 
-        public int Init(IDispatch pBackConnection)
+        public void Init([MarshalAs(UnmanagedType.IDispatch)][In] object pBackConnection)
         {
-            return S_OK;
+            //return S_OK;
         }
 
-        public int Done()
+        public void Done()
         {
-            return S_OK;
+            //return S_OK;
         }
 
-        public int GetInfo([MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_I4)] ref IntPtr pInfo)
+        public void GetInfo([MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref object[] pInfo)
         {
-            // defererencing double pointer to actual pointer - not needed since we're accepting ref ptr
-            //var deref1 = (IntPtr)Marshal.PtrToStructure(pInfo, typeof(IntPtr));
+            pInfo[0] = 2000;
 
-            // dereferencing pointer to actual int[]
-            var deref2 = (int[])Marshal.PtrToStructure(pInfo, typeof(int[]));
-
-            deref2[0] = 2000;
-
-            Marshal.StructureToPtr(deref2, pInfo, false);
-
-            return S_OK;
+            //return S_OK;
         }
-
-    }
-
-
-    [Guid("A653C8C0-7D93-4B85-B503-57665E2D7A93")]
-    public interface DSReader_Events
-    {
 
     }
 }
